bd2396727c6c1468067ce33a4d3e11bb7d8cb5a0
gxy <plerks@163.com>
2025-04-30 22:54:14 +0800

    修正stride算法的错误实现 见ch5分支，commit_id d286dfa2331a23b89e4dccba41cf8a559e74bf1d

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -34,19 +34,33 @@ impl TaskManager {
     }
 
     /// Take a process out of the ready queue
-    /// 比较两个stride时要化为有符号数进行比较，具体原因为：
+    /// 我们要比较两个stride值的大小，但是stride值随着不断+pass，是有可能溢出的，如何处理？
     /// 首先，参考[rcore-camp-guide](https://learningos.cn/rCore-Camp-Guide-2025S/chapter5/4exercise.html)，
-    /// stride 调度要求进程优先级 >= 2，初始各进程stride值都为0，每次选最小的stride出来，所以系统变化过程中会维持：
+    /// stride 调度要求进程优先级 >= 2，初始各进程stride值都为0，每次选最小的stride出来，加上步进值pass = BigStride / priority，
+    /// 所以系统变化过程中会维持：
     /// STRIDE_MAX – STRIDE_MIN <= BigStride / 2
-    /// 我们如何在可能发生溢出的情况下正确比较stride大小？
-    /// 有符号数构成一个环 10000000 10000001 ... 00000000 ... 00000001 .. 01111111
-    /// 而由STRIDE_MAX – STRIDE_MIN <= BigStride / 2，所有进程的stride范围这个滑窗的长度最大也就BigStride的一半，BigStride可以设为255，
-    /// 若将stride保持为无符号数，<的比较在窗口stride值全为负/在0附近(有正有负)/全为正的情况下，都能给出正确结果，
-    /// 但是对于像 11111110 11111111 00000000 00000001 的情况，两个进程stride值`{11111110, 11111111}` -> `{(1)00000000, 11111111}`，
-    /// 无符号数比较会认为00000000小从而调度运行它，实际上00000000是变大越界的，
-    /// 其代表的值其实是 11111111 右边的一个大值，应该运行 11111111
-    /// 所以，要把stride值转为有符号比较，各类情况下<比较都能给出正确结果。
-    /// 通过这种方式，本来要无限位长来记stride值，现在不用了。
+    /// 在没有发生溢出的情况下，我们是能正确比较stride值的大小的，但是问题在于进程运行时间久了stride值可能溢出，
+    /// 例如当两个进程stride值`{1111_1110, 1111_1111}` -> `{(1)0000_0000, 1111_1111}`时(第一个stride值加了pass 2)，
+    /// 无符号数比较会认为0000_0000小从而调度运行它，实际上0000_0000是变大越界的数。
+    /// 把stride值当成有符号数来比较也是不行的，虽然上面的边界情况能处理，但是对于`{0111_1110, 0111_1111}` -> `{1000_0000, 0111_1111}`，
+    /// 会继续选择1000_0000出来运行。
+    /// 
+    /// 那么，怎样的策略才能使得即便考虑溢出，我们也能正确进行stride值的比较？
+    /// 
+    /// 机器数构成一个环 0000_0000 ... 0111_1111 1000_0000 ... 1111_1111
+    /// 在环上，stride值都是顺时针在跑（stride值增大），只要不超过一整圈，两个stride值a, b相减就是他们之间的距离（可正可负），
+    /// （一圈是指例如stride是u8，所有u8的数构成一个环）
+    /// 于是我们考虑 a - b，把 a - b 转成有符号数，判断正负即可知道到底是a在前面还是b在前面，(也可以不转有符号数，直接判断最高位是0/1)
+    /// 但是这里还有个问题，a - b 不能溢出符号位，否则判断 a - b 的正负会有问题。也就是说a不能在b前面太多，a如果在b前面1000_0000步，
+    /// 我们会误认为这是个负数，会误以为a在b后面。
+    /// 而由STRIDE_MAX – STRIDE_MIN <= BigStride / 2，BigStride可以设为255，即保证了 a - b 不会溢出符号位，
+    /// 也保证了不会超过一整圈。
+    /// 为什么BigStride要取上界？
+    /// 不然的话有效的priority取值少，例如BigStride是3，那么不管priority怎么取，BigStride / priority只会是 1 或者 0
+    /// 
+    /// 总结一句话：算 a - b 的正负，且要控制差值不能溢出符号位。
+    /// 
+    /// 通过判断 (signed)(a - b) 的正负，本来要无限位长来记stride值，现在不用了。
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
         if self.ready_queue.is_empty() {
             return None;
@@ -57,7 +71,7 @@ impl TaskManager {
         // stride 算法要找到 stride 最小的进程，使用优先级队列是效率不错的办法，但是我们的实验测例很简单，所以效率完全不是问题。事实上，很推荐使用暴力扫一遍的办法找最小值。
         for i in 1..self.ready_queue.len() {
             let stride = self.ready_queue[0].as_ref().inner_exclusive_access().stride;
-            if (stride as i8) < (min_stride as i8) { // 转为有符号数来比较
+            if ((stride - min_stride) as i8) < 0 { // 判断(signed)(a - b) 的正负
                 index = i;
                 min_stride = stride;
             }
